01faf98a576974e7392d2c4e4c89e2d0310df076
Ganesh Pawar <ganesh.pawar@vernost.com>
2026-01-29 15:21:58 +0530

    changes added

---
+++ findWordsContaining.js
@@ -0,0 +1,13 @@
+
+var findWordsContaining = function(words, x) {
+    let result = [];
+    for(let i = 0; i < words.length; i++){
+        for(let j = 0; j < words[i].length; j++){
+            if(x === words[i][j]){
+                result.push(i);
+                break;
+            }
+        }
+    }
+    return result;
+};
