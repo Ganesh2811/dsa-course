3d6d567ede588d97f5fd8dea7b411b02cf34d8c8
Ganesh2811 <gp11282001@gmail.com>
2026-04-13 17:55:42 +0530

    changes added

---
+++ promise.js
@@ -0,0 +1,14 @@
+// promise is usally used for an asyncrouns operation like api calls, file loading and timer delays
+// Pending: The task is in the initial state.
+// Fulfilled: The task was completed successfully, and the result is available.
+// Rejected: The task failed, and an error is provided.
+
+function loadData(callback){
+    setTimeout(() => callback("Data loaded"), 1000);
+}   
+
+function prmisefullfilled(){
+    return new Promise((resolve) => { loadData((result) => resolve(result)) })
+}
+
+prmisefullfilled().then((data) => console.log(data));
