487c47aa4f467ce2f72936e1a570f27f104d4179
Ganesh2811 <gp11282001@gmail.com>
2026-04-15 14:50:30 +0530

    changes added

--- twoSum.js
+++ twoSum.js
@@ -1,7 +1,7 @@
 
 function twoSum(nums, target){
     const map = new Map();
-    for(let i=0; i < nums.length; i++){
+    for(let i = 0; i < nums.length; i++){
         const complement = target - nums[i];
         if(map.has(complement)){
             return [map.get(complement), i]
